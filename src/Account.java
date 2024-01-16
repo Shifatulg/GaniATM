bf94baeefbd7f377b78074a9504a51f2b8959e63
BT_1N3_12 <sample@bths.edu>
2024-01-16 09:14:58 -0500

    ATM and Customer wip

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,20 @@
 public class Account {
+    private int balance;
+    private boolean savings;
+    public Account(boolean checkings) {
+        if (checkings) {
+            savings = false;
+        } else {
+            savings = true;
+        }
+        balance = 0;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
 }
--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
 public class Customer {
+    private static Scanner scan = new Scanner(System.in);
+    private int pin;
+    private String name;
+    private Account checkings;
+    private Account savings;
 
+    public Customer() {
+        System.out.println("What is your name?");
+        name = scan.nextLine();
+        System.out.println("What is your pin?");
+        pin = scan.nextInt();
+        checkings = new Account(true);
+        savings = new Account(false);
+    }
 }
