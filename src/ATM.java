a6279732b42d78b6aa358d7ca7703205b63be00c
BT_1N3_12 <sample@bths.edu>
2024-01-17 09:14:58 -0500

    UI improved and created

--- src/ATM.java
+++ src/ATM.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
 public class ATM {
+    private static final Scanner s = new Scanner(System.in);
+    Customer man = new Customer();
+    public ATM() { }
 
+    public void start() {
+
+    }
+
+    private void introduce() {
+        System.out.println("Hello" + man.getName() + "welcome to the ATM");
+    }
+
+    private void mainMenu() {
+        System.out.println("Which account would you like to use, (1) checkings, (2) savings" );
+        int choice = s.nextInt();
+        System.out.println("---------------------");
+        System.out.println("1. Withdraw Money");
+        System.out.println("2. Deposit money");
+        System.out.println("3. Transfer money between accounts");
+        System.out.println("4. Get account balances");
+        System.out.println("5. Get transaction history");
+        System.out.println("6. Change PIN");
+        System.out.println("7. Exit");
+        System.out.println("---------------------");
+    }
 }
--- src/Customer.java
+++ src/Customer.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Customer {
-    private static Scanner scan = new Scanner(System.in);
+    private static final Scanner scan = new Scanner(System.in);
     private int pin;
     private String name;
     private Account checkings;
@@ -13,5 +13,18 @@ public class Customer {
         pin = scan.nextInt();
         checkings = new Account(true);
         savings = new Account(false);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPin(int newPin) {
+        pin = newPin;
+    }
+    public boolean enterPin(int attempt) {
+        if (pin == attempt) {
+            return true;
+        } return false;
     }
 }
